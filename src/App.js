9384c6de274b8264d8acf50e7833d9eb7dcf193d
elisha111 <elisey.kovalyov@yandex.ru>
2025-04-29 00:52:48 +0300

    #12 знакомсто с Redux Toolkit

--- src/App.js
+++ src/App.js
@@ -8,10 +8,16 @@ import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import NotFound from "./pages/NotFound";
 
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./redux/slices/filterSlise";
+
 export const SearchContext = React.createContext("");
 
 function App() {
   const [searchValue, setSearchValue] = React.useState("");
+
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
 
   return (
     <div className="wrapper">
@@ -25,6 +31,24 @@ function App() {
           </Routes>
         </div>
       </SearchContext.Provider>
+
+      <div>
+        <div>
+          <button
+            aria-label="Increment value"
+            onClick={() => dispatch(increment())}
+          >
+            Increment
+          </button>
+          <span>{count}</span>
+          <button
+            aria-label="Decrement value"
+            onClick={() => dispatch(decrement())}
+          >
+            Decrement
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
