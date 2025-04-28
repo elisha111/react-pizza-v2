dc7b912153b5e19ebbe04aa0125303de5f2f6fb9
Елисей Ковалев <kovalev.e@twinscorp.ru>
2025-04-28 16:22:15 +0300

    #11 избавление от Props Drilling/использование useContext

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,10 @@ import Sort from "../components/Sort";
 import PizzaBlock from "../components/PizzaBlock";
 import Skeleton from "../components/PizzaBlock/Skeleton";
 import Pagination from "../components/Pagination";
+import { SearchContext } from "../App";
 
-const Home = ({ searchValue }) => {
+const Home = () => {
+  const { searchValue } = React.useContext(SearchContext);
   // пагинация
   const [currentPage, setCurrentPage] = React.useState(1);
   // сортировка
